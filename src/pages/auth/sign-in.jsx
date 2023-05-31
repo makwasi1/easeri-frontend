908d1e4f5a92995c765397321eff0e3c2f35ebaa
makwasi1 <crismakwasi12@gmail.com>
2023-05-31 14:12:34 +0300

    sign in url

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -26,7 +26,7 @@ export function SignIn() {
 
   const login = async (e) => {
     e.preventDefault();
-    let loginURL = "/api/users/login/";
+    let loginURL = "https://easeri-backend-production.up.railway.app/api/users/login/";
     const response = await axios.post(
         loginURL, {
           "username": email, "password": password
